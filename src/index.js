2d853f74989afd3650f2fd5b78dbaf24acc72a16
tagawa shouta <sho.webfe@gmail.com>
2023-01-06 05:27:42 +0900

    swaggerをmockに移動

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ const path = require('path');
 const getComponent = require('./lib/getComponent');
 
 try {
-  const doc = yaml.load(fs.readFileSync(path.resolve(__dirname, 'swagger', 'index.yml'), 'utf8'));
+  const doc = yaml.load(fs.readFileSync(path.resolve(__dirname, 'mock', 'swagger.yml'), 'utf8'));
   const paths = doc.paths;
   const put = paths['/pet']['put'];
   const responseRef = put.responses['200'].content['application/json'].schema.$ref;
   const component = getComponent(doc, responseRef);
-  console.log(component)
 } catch (e) {
   throw e;
 }
